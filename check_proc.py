2d1c06538ce9debaa2ea1bfbbe887a1560c90ad3
Josh Martinez <josh.martinez@bird.co>
2020-02-16 14:28:09 -0800

    added logging

--- check_proc.py
+++ check_proc.py
@@ -1,20 +1,28 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from pytz import timezone
 import subprocess
+import logging
 import requests
 import socket
 import json
 import os
 
-INTERVAL = 300  # SECONDS
-PD_URL = 'https://events.pagerduty.com/v2/enqueue'
-HOST = socket.getfqdn()  # Were this to change, it would require the script be restarted
-TZ = timezone('America/Los_Angeles')
 
+HOST = socket.getfqdn()  # Were this to change suddenly, it would require the script be restarted
+TZ = timezone('America/Los_Angeles')
 PROC_NAME = os.environ['PROC_NAME']
 INT_KEY = os.environ['INT_KEY']
+PD_URL = 'https://events.pagerduty.com/v2/enqueue'
 
-dedup_key = None  # This also keeps track of whether or not an incident was triggered successfully
+# The only configurable "setting". 5 minutes seems fine for now.
+INTERVAL = 300  # SECONDS
+
+# This also keeps track of whether or not an incident was triggered successfully
+dedup_key = None
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s')
 
 
 def monitor_tester():
@@ -24,10 +32,10 @@ def monitor_tester():
         "payload": {
             "severity": "info",
             "source": HOST,
-            "summary": f"[INTEGRATION TEST] process monitor for {PROC_NAME} on {HOST}",  # MAKE ME AN f-string!
+            "summary": f"[INTEGRATION TEST] process monitor for {PROC_NAME} on {HOST}",
             "custom_details": {
                 "info": (
-                    f"This is an integration test for the monitoring of {PROC_NAME} on {HOST}. "  # MAKE ME AN f-string!
+                    f"This is an integration test for the monitoring of {PROC_NAME} on {HOST}. "
                     "It will trigger once when the integration comes online and subsequently "
                     "every first day of the month at 17:00.")
             }
@@ -37,10 +45,10 @@ def monitor_tester():
     try:
         response = requests.post(PD_URL, data=json.dumps(trigg_payload))
         dedup_key = response.json()["dedup_key"]
-        print(response.status_code, f'Trigerred INTEGRATION TEST {dedup_key}')
+        logging.info(response.status_code, f'Trigerred INTEGRATION TEST {dedup_key}')
 
     except requests.exceptions.RequestException as e:
-        print(e, 'Unable to trigger check!')
+        logging.error(e, 'Unable to trigger check!')
         return  # No point in continuing if this is the case
 
     ack_payload = {
@@ -51,12 +59,10 @@ def monitor_tester():
 
     try:
         response = requests.post(PD_URL, data=json.dumps(ack_payload))
-        print(response.status_code, f'Acknowledged {dedup_key}')
+        logging.info(response.status_code, f'Acknowledged {dedup_key}')
 
     except requests.exceptions.RequestException as e:
-        print(e, 'Unable to ack check!')
-
-    print()  # Space is the essence of Zen
+        logging.error(e, 'Unable to ack check!')
 
 
 def trigger(dedup_key):
@@ -81,11 +87,11 @@ def trigger(dedup_key):
     try:
         response = requests.post(PD_URL, data=json.dumps(payload))
         current_dedup = response.json()["dedup_key"]
-        print(response.status_code, f'Triggered/retriggered with {current_dedup}')
+        logging.info(response.status_code, f'Triggered/retriggered with {current_dedup}')
         return current_dedup
 
     except requests.exceptions.RequestException as e:
-        print(e, 'Unable to trigger/retrigger incident!')
+        logging.error(e, 'Unable to trigger/retrigger incident!')
         return None
 
 
@@ -98,11 +104,11 @@ def resolve(dedup_key):
 
     try:
         response = requests.post(PD_URL, data=json.dumps(payload))
-        print(response.status_code, f'Resolved {dedup_key}')
+        logging.info(response.status_code, f'Resolved {dedup_key}')
         return True
 
     except requests.exceptions.RequestException as e:
-        print(e, 'Unable to resolve incident!')
+        logging.error(e, 'Unable to resolve incident!')
         return False
 
 
@@ -111,9 +117,9 @@ def check_proc_running(proc_name):
 
     if stdout:
         pids_list = stdout.decode('utf-8').strip().split('\n')
-        print(f'Found {proc_name} with PID(s): ' + ', '.join(pids_list))
+        logging.info(f'Found {proc_name} with PID(s): ' + ', '.join(pids_list))
     else:
-        print(f'No PIDs found for {proc_name}')
+        logging.info(f'No PIDs found for {proc_name}')
 
     return bool(stdout)  # Simple enough. Empty string means its not running.
 
@@ -125,7 +131,7 @@ def monitor():
 
     # If it's NOT running, go ahead and trigger/retrigger
     if not running:
-        print(f'{PROC_NAME} is NOT running. Attempting trigger/retrigger...')
+        logging.info(f'{PROC_NAME} is NOT running. Attempting trigger/retrigger...')
 
         new_dedup = trigger(dedup_key)
 
@@ -134,7 +140,7 @@ def monitor():
 
     # If it is running and dedup_key is set, that means it needs to be resolved
     elif dedup_key:
-        print(f'{PROC_NAME} is running again. Attempting to resolve incident...')
+        logging.info(f'{PROC_NAME} is running again. Attempting to resolve incident...')
 
         resolved = resolve(dedup_key)
 
@@ -143,9 +149,9 @@ def monitor():
             dedup_key = None
 
     else:
-        print(f'{PROC_NAME} appears to be running and incident is untriggered')
+        logging.info(f'{PROC_NAME} appears to be running and incident is untriggered')
 
-    print(f'Checking again in {INTERVAL} seconds.\n')
+    logging.info(f'Checking again in {INTERVAL} seconds.\n')
 
 
 def scheduler():
@@ -154,7 +160,7 @@ def scheduler():
     sched.add_job(monitor_tester, 'cron', day='01', hour='17', timezone=TZ)
     sched.add_job(monitor, 'interval', seconds=INTERVAL)
 
-    print('Starting scheduler')
+    logging.info('Starting scheduler')
     sched.start()
 
 
@@ -163,4 +169,4 @@ if __name__ == '__main__':
         monitor_tester()  # Initial "gauge sweep"
         scheduler()
     except KeyboardInterrupt:
-        print("Ending Scheduler")
+        logging.info("Ending Scheduler")
