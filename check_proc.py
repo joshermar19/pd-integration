d3a498a949d9338c101a6215d88d930237cd046c
Josh Martinez <josh.martinez@bird.co>
2020-02-16 22:51:39 -0800

    added a whole lotta ec2 metadata to payload

--- check_proc.py
+++ check_proc.py
@@ -3,16 +3,14 @@ from pytz import timezone
 import subprocess
 import logging
 import requests
-import socket
 import json
 import os
 
 
-HOST = socket.getfqdn()  # Were this to change suddenly, it would require the script be restarted
 TZ = timezone('America/Los_Angeles')
-PROC_NAME = os.environ['PROC_NAME']
-INT_KEY = os.environ['INT_KEY']
 PD_URL = 'https://events.pagerduty.com/v2/enqueue'
+INT_KEY = os.environ['INT_KEY']
+PROC_NAME = os.environ['PROC_NAME']
 
 # The only configurable "setting". 5 minutes seems fine for now.
 INTERVAL = 300  # SECONDS
@@ -25,24 +23,54 @@ logging.basicConfig(
     format='%(asctime)s [%(levelname)s] %(message)s')
 
 
-def monitor_check():
-    logging.info('Commencing INTEGRATION CHECK...')
+def _get_ec2_metadata(attribute):
+    url = f'http://169.254.169.254/latest/meta-data/{attribute}'
+    try:
+        response = requests.get(url, timeout=2)
+        if response.status_code != 200:
+            logging.warning(f"{response.status_code} on {url}")
+            return None
+        return response.text
+    except requests.exceptions.RequestException as e:
+        logging.warning(f"Could not read EC2 attribute {attribute} {e}")
+        return None
 
-    trigg_payload = {
+
+def get_trigger_payload(summary, info, severity="critical"):
+    instance_id = _get_ec2_metadata("instance-id")
+    local_ipv4 = _get_ec2_metadata("local-ipv4")
+    local_hostname = _get_ec2_metadata("local-hostname")
+    public_ipv4 = _get_ec2_metadata("public-ipv4")
+    public_hostname = _get_ec2_metadata("public-hostname")
+
+    return {
         "event_action": "trigger",
         "routing_key": INT_KEY,
         "payload": {
-            "severity": "info",
-            "source": HOST,
-            "summary": f"[INTEGRATION CHECK] process monitor for {PROC_NAME} on {HOST}",
+            "severity": severity,
+            "source": instance_id,
+            "summary": summary.format(PROC_NAME=PROC_NAME, instance_id=instance_id),
             "custom_details": {
-                "info": (
-                    f"This is an integration check for the monitoring of {PROC_NAME} on {HOST}. "
-                    "It will trigger once when the integration comes online and subsequently "
-                    "every first day of the month at 17:00.")
+                "info": info.format(PROC_NAME=PROC_NAME, instance_id=instance_id, INTERVAL=INTERVAL),
+                "process name": PROC_NAME,
+                "instance_id": instance_id,
+                "local_ipv4": local_ipv4,
+                "local_hostname": local_hostname,
+                "public_ipv4": public_ipv4,
+                "public_hostname": public_hostname
             }
         }
     }
+
+
+def monitor_check():
+    logging.info('Commencing integration check...')
+
+    trigg_summary = "[INTEGRATION CHECK] process monitor for {PROC_NAME} on {instance_id}"
+    trigg_info = ("This is an integration check for the monitoring of {PROC_NAME} on {instance_id}. "
+                  "It will trigger once when the integration comes online and subsequently "
+                  "every first day of the month at 17:00.")
+    trigg_payload = get_trigger_payload(trigg_summary, trigg_info, severity="info")  # Remember, "critical" severity is the default
 
     try:
         response = requests.post(PD_URL, data=json.dumps(trigg_payload))
@@ -68,26 +96,16 @@ def monitor_check():
 
 
 def trigger(dedup_key):
-    payload = {
-        "event_action": "trigger",
-        "routing_key": INT_KEY,
-        "payload": {
-            "severity": "critical",
-            "source": HOST,
-            "summary": f"{PROC_NAME} has stopped running on {HOST}",
-            "custom_details": {
-                "info": (
-                    f"{PROC_NAME} was not running on {HOST} within the last {INTERVAL} seconds. "
-                    "Please reach out to service owner for troubleshooting.")
-            }
-        }
-    }
+    trigg_summary = "{PROC_NAME} has stopped running on {instance_id}"
+    trigg_info = ("{PROC_NAME} was not running on {instance_id} within the last {INTERVAL} seconds. "
+                  "Please reach out to service owner for troubleshooting.")
+    trigg_payload = get_trigger_payload(trigg_summary, trigg_info)
 
-    if dedup_key:  # If not, this is the first trigger and no dedup_key should be sent
-        payload["dedup_key"] = dedup_key
+    if dedup_key:  # If this is the first trigger (dedup_key=None), then no dedup is appended to payload
+        trigg_payload["dedup_key"] = dedup_key
 
     try:
-        response = requests.post(PD_URL, data=json.dumps(payload))
+        response = requests.post(PD_URL, data=json.dumps(trigg_payload))
         current_dedup = response.json()["dedup_key"]
         logging.info(f'{response.status_code} Successfully triggered/retriggered with dedup_key {current_dedup}')
         return current_dedup
@@ -98,14 +116,14 @@ def trigger(dedup_key):
 
 
 def resolve(dedup_key):
-    payload = {
+    resolve_payload = {
         "event_action": "resolve",
         "routing_key": INT_KEY,
         "dedup_key": dedup_key
     }
 
     try:
-        response = requests.post(PD_URL, data=json.dumps(payload))
+        response = requests.post(PD_URL, data=json.dumps(resolve_payload))
         logging.info(f'{response.status_code} Successfully resolved incident with dedup_key: {dedup_key}')
         return True
 
