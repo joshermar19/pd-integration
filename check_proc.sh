7e6c321ecb3d0631c8cbc75d4b9f04a0c6042d02
Josh Martinez <josh.martinez@bird.co>
2020-02-16 16:13:02 -0800

    removed psutil from requests and replaced SOURCE with . in .sh file

--- check_proc.sh
+++ check_proc.sh
@@ -13,14 +13,14 @@ fi
 # Check for virtual environment and set up if necessary
 if [ -f ./env/bin/activate ]; then
 	echo 'found ./env/bin/activate'
-	source ./env/bin/activate
+	. ./env/bin/activate
 	python3 check_proc.py
 
 else
 	echo 'Could not find ./env/bin/activate'
 	echo 'Setting up virtual venv...'
 	python3 -m venv env
-	source ./env/bin/activate
+	. ./env/bin/activate
 	pip install -r requirements.txt
 	python3 check_proc.py
 fi
